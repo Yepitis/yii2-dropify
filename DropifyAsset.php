23188d9a73166138e75f014703c2ee38981e9aa7
张欣宇 <176956856@qq.com>
2017-06-14 16:25:56 +0800

    添加ASSET类。

---
+++ DropifyAsset.php
@@ -0,0 +1,28 @@
+<?php
+namespace AprSoft\Dropify;
+
+use yii\web\AssetBundle;
+
+/**
+ * @todo Class Dropify Asset
+ * @package AprSoft\Dropify
+ */
+class DropifyAsset extends AssetBundle
+{
+    public $sourcePath;
+    public $js = [
+        'js/dropify.min.js',
+    ];
+    public $css = [
+        'css/dropify.min.css',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\web\JqueryAsset',
+    ];
+    public function init()
+    {
+        $this->sourcePath = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'assets';
+        parent::init();
+    }
+}
